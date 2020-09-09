732ae4531a5845659782d774173dbf02c86a605d
unknown <jungwookim37@hanmail.net>
2020-09-09 15:08:15 +0900

    update

--- src/main/java/cnabookstore/DeliveryApplication.java
+++ src/main/java/cnabookstore/DeliveryApplication.java
@@ -2,6 +2,7 @@ package cnabookstore;
 import cnabookstore.config.kafka.KafkaProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.ApplicationContext;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableBinding(KafkaProcessor.class)
 @EnableFeignClients
+@EnableCircuitBreaker
 public class DeliveryApplication {
     protected static ApplicationContext applicationContext;
     public static void main(String[] args) {
--- src/main/java/cnabookstore/DeliveryController.java
+++ src/main/java/cnabookstore/DeliveryController.java
@@ -1,15 +1,68 @@
 package cnabookstore;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Optional;
 
- @RestController
+@RestController
  public class DeliveryController {
 
+  @Autowired
+  DeliveryRepository deliveryRepository;
+
+  @GetMapping("/circuitBreaker")
+  @HystrixCommand(fallbackMethod = "fallback", commandProperties = {
+          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+//          @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000"),
+//          @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10"),
+//          @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+          @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000")
+  })
+  public String circuitBreakerTest(@RequestParam String isYn) throws InterruptedException {
+
+   if (isYn.equals("Y")) {
+    System.out.println("@@@ CircuitBreaker!!!");
+    Thread.sleep(10000);
+    //throw new RuntimeException("CircuitBreaker!!!");
+   }
+
+   System.out.println("$$$ SUCCESS!!!");
+   return " SUCCESS!!!";
+  }
+
+  @GetMapping("/selectDeliveryInfo")
+  @HystrixCommand(fallbackMethod = "fallbackDelivery", commandProperties = {
+          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+          @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000")
+  })
+  public String selectDeliveryInfo(@RequestParam long deliveryId) throws InterruptedException {
+
+   if (deliveryId <= 0) {
+    System.out.println("@@@ CircuitBreaker!!!");
+    Thread.sleep(10000);
+    //throw new RuntimeException("CircuitBreaker!!!");
+   } else {
+    Optional<Delivery> delivery = deliveryRepository.findById(deliveryId);
+    return delivery.get().getDeliveryStatus();
+   }
+
+   System.out.println("$$$ SUCCESS!!!");
+   return " SUCCESS!!!";
+  }
+
+  private String fallback(String isYn) {
+   System.out.println("### fallback!!!");
+   return "CircuitBreaker!!!";
+  }
+
+ private String fallbackDelivery(long deliveryId) {
+  System.out.println("### fallback!!!");
+  return "CircuitBreaker!!!";
+ }
  }
